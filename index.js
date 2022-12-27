163abcd43755b7ad5e34b76d349330388f871a15
Willian Figueiredo <williansfigueiredos@hotmail.com>
2022-12-27 01:16:20 -0300

    1.0

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 function openNav() {
+    
     document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
+
     document.getElementById("mySidenav").style.width = "0";
 }
+
+/* 
+function updateImage() {
+
+    if (window.matchMedia("(min-width: 500px)").matches) {
+        
+    document.getElementById("pic3").src = "Fotos/download__1_-removebg-preview.png";
+
+    
+    } else {
+
+    document.getElementById("pic3").src = "Fotos/download__1_-removebg-preview22.png";
+    }
+}
+
+window.addEventListener("resize", updateImage);
+updateImage();
+*/
+
+
+function startCarousel() {
+    const carousel = document.getElementById("carouselExampleCaptions");
+    const nextButton = carousel.querySelector(".carousel-control-next");
+
+    setInterval(function() {
+    nextButton.dispatchEvent(new Event("click"));
+    }, 5000);
+}
+
+  startCarousel(); 
+
+
+
+
+
+
+
+
+
+
 

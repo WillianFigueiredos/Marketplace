314f9687c6f055ffc30afdd407b2e63a971cb1d4
Willian Figueiredo <williansfigueiredos@hotmail.com>
2023-01-06 20:19:00 -0300

    1.0

---
+++ assets/JS/login.js
@@ -0,0 +1,39 @@
+
+  function toggleDropdownMenu() {
+    var menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+  function toggleDropdownMenu2() {
+    var menu = document.getElementById("dropdown-menu2");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+  function toggleDropdownMenu3() {
+    var menu = document.getElementById("dropdown-menu3");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+  function toggleDropdownMenu4() {
+    var menu = document.getElementById("dropdown-menu4");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+  
+

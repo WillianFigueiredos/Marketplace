5fd396e72ea017edbc0ea79ddae6629d480a7c77
Willian Figueiredo <williansfigueiredos@hotmail.com>
2023-01-10 11:47:12 -0300

    1.0

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -133,18 +133,111 @@ function redirectProductpage() {
 
 
 
+
+var navbar1 = document.querySelector("#navbar1");
+var navbar2 = document.querySelector("#navbar2");
+
+
+var sticky = navbar1.offsetTop;
+
+
 window.onscroll = function() {
-  fixNavbar()
-};
-
-var navbar = document.getElementsByClassName("navbar")[0];
-
-var sticky = navbar.offsetTop;
-
-function fixNavbar() {
+ 
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("fixed");
+    
+    navbar1.classList.add("fixed");
+    navbar2.classList.add("fixed");
+    navbar2.classList.add("color");
   } else {
-    navbar.classList.remove("fixed");
+    
+    navbar1.classList.remove("fixed");
+    navbar2.classList.remove("fixed");
+    navbar2.classList.remove("color");
   }
-}
+
+}; 
+
+
+// Contador Desktop
+const countDownDate = new Date("Jan 20, 2023 19:00:00").getTime();
+
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+const dayLabel = document.getElementById("day-label");
+const hourLabel = document.getElementById("hours-label");
+const minutesLabel = document.getElementById("minutes-label");
+const secondsLabel = document.getElementById("seconds-label");
+
+const x = setInterval(function() {
+
+  const now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  if (distance < 0) {
+    distance = countDownDate - distance;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  daysElement.innerHTML = days;
+  hoursElement.innerHTML = hours;
+  minutesElement.innerHTML = minutes;
+  secondsElement.innerHTML = seconds;
+  dayLabel.innerHTML = "DIA";
+  hourLabel.innerHTML = "HORAS";
+  minutesLabel.innerHTML = "MIN.";
+  secondsLabel.innerHTML = "SEG.";
+
+}, 1000);
+
+console.log(countDownDate);
+
+
+
+// Contador Mobile
+const countDownDatemobile = new Date("Jan 20, 2023 19:00:00").getTime();
+
+const daysMobileElement = document.getElementById("days-mobile");
+const hoursMobileElement = document.getElementById("hours-mobile");
+const minutesMobileElement = document.getElementById("minutes-mobile");
+const secondsMobileElement = document.getElementById("seconds-mobile");
+const dayLabelMobile = document.getElementById("day-label-mobile");
+const hourLabelMobile = document.getElementById("hours-label-mobile");
+const minutesLabelMobile = document.getElementById("minutes-label-mobile");
+const secondsLabelMobile = document.getElementById("seconds-label-mobile");
+
+
+const xmobile = setInterval(function() {
+
+  const now = new Date().getTime();
+
+  let distance = countDownDatemobile - now;
+
+  if (distance < 0) {
+    distance = countDownDatemobile - distance;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  daysMobileElement.innerHTML = days;
+  hoursMobileElement.innerHTML = hours;
+  minutesMobileElement.innerHTML = minutes;
+  secondsMobileElement.innerHTML = seconds;
+  dayLabelMobile.innerHTML = "DIAS";
+  hourLabelMobile.innerHTML = "HORAS";
+  minutesLabelMobile.innerHTML = "MIN.";
+  secondsLabelMobile.innerHTML= "SEG.";
+  
+  
+}, 1000);
+
+console.log(countDownDate);

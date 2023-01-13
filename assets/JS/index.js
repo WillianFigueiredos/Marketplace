cdfd23907bfc0e3d961b5ffacfebdd6cc7b17ccb
Willian Figueiredo <williansfigueiredos@hotmail.com>
2023-01-13 16:16:45 -0300

    1.0

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function()
   document.querySelector(".spinner-wrapper").style.opacity= "0";
   setTimeout(function(){
     document.querySelector(".spinner-wrapper").style.display = "none";
-  }, 3000); 
+  }, 2000); 
 });
 
 
